cf5b7e16396e4cc269a20845a4b584a3e475244f
Documenter.jl <documenter@juliadocs.github.io>
2022-12-07 06:00:42 +0000

    build based on 76c75fb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WASMCompiler","category":"page"},{"location":"#WASMCompiler","page":"Home","title":"WASMCompiler","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WASMCompiler.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WASMCompiler]","category":"page"}]
+}
